ac79863c2a9500a22514cc233ee50a2b60ec7f02
daneng1 <engeldb@gmail.com>
2023-08-19 07:50:54 -0700

    Initial Commit

---
+++ ColorChange/ColorChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeApp.swift
+//  ColorChange
+//
+//  Created by Dan and Beth Engel on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
